71b3b2819412c033789def9a1a2a8c5ba9e07136
tjacek <tjacek@student.agh.edu.pl>
2015-09-27 19:16:44 +0200

    normalize actions

--- preproc/normalize.py
+++ preproc/normalize.py
@@ -4,12 +4,14 @@ import scipy.misc as image
 import numpy as np
 from sklearn import preprocessing
 
-def normalize_images(in_path,frame_path):
+def normalize_images(in_path,frame_path,action_path):
     actions=utils.get_all_dirs(in_path)
     actions=utils.append_path(in_path,actions)
     images,image_names,dim=get_raw_images(actions)
     images=utils.normalize(images)
+    images=map(lambda x:x.reshape(dim),images)
     save_frames(images,image_names,frame_path,dim)    
+    save_actions(action_path,images,image_names,actions)
 
 def get_raw_images(actions):
     images=[]
@@ -30,11 +32,24 @@ def get_raw_images(actions):
 def save_frames(images,image_names,frame_path,dim):
     for i,img in enumerate(images):
         full_path=frame_path+image_names[i]
-        img=np.reshape(img,dim)
-        print(img.shape)
         image.imsave(full_path,img)
+
+def save_actions(out_path,images,image_names,actions):
+    for action_path in actions:
+        action_name=utils.get_filename(action_path)
+        dir_path=out_path+action_name+"/"
+        utils.make_dir(dir_path)
+        print(dir_path)
+        frame_names=utils.get_all_files(action_path)
+        for frame_name in frame_names:
+            print(frame_name)
+            index=image_names.index(frame_name)
+            frame=images[index]
+            full_path=dir_path+frame_name
+            image.imsave(full_path,frame)
        
 if __name__ == "__main__":
    in_action="/home/user/df/input/raw_actions/"
    frame_path="/home/user/df/input/frames/"
-   normalize_images(in_action,frame_path)
+   out_action="/home/user/df/input/actions/"
+   normalize_images(in_action,frame_path,out_action)
--- utils.py
+++ utils.py
@@ -7,40 +7,27 @@ import numpy as np
 import scipy.misc as image
 from sklearn import preprocessing
 
+#basic io functions
 def get_all_files(path):
     all_files= [ f for f in listdir(path) if isfile(join(path,f)) ]
     all_files.sort()
     return all_files
 
 def get_all_dirs(path):
-    all_files= [ f for f in listdir(path) if not isfile(join(path,f)) ]
-    all_files.sort()
-    return all_files
+    all_dirs= [ f for f in listdir(path) if not isfile(join(path,f)) ]
+    all_dirs.sort()
+    return all_dirs
 
-def append_path(path,files):
-    return map(lambda f: path+f,files)
-
-def array_to_txt(array):
-    return reduce(lambda x,y:x+str(y),array,"")
-
-def normalize(data):
-    if(isinstance(data,list)):
-        data=np.array(data)
-    data=data.astype(float)
-    min_max_scaler = preprocessing.MinMaxScaler()
-    data = min_max_scaler.fit_transform(data)
-    return data
+def read_file(path):
+    file_object = open(path,'r')
+    lines=file_object.readlines()  
+    file_object.close()
+    return lines
 
 def save_string(path,string):
     file_str = open(path,'w')
     file_str.write(string)
     file_str.close()
-
-def read_file(path):
-    file_object = open(path,'r')
-    obj=file_object.readlines()  
-    file_object.close()
-    return obj
 
 def save_object(path,nn):
     file_object = open(path,'wb')
@@ -53,15 +40,16 @@ def read_object(path):
     file_object.close()
     return obj
 
-def to_csv_file(path,vectors):
-    file_csv = open(path,'w')
-    csv=""
-    for i,instance in enumerate(vectors):
-        print(i)
-        str_v=vector_to_str(instance)
-        csv+=str_v
-    file_csv.write(csv)
-    file_csv.close()
+def make_dir(path):
+    if(not os.path.isdir(path)):
+	os.system("mkdir "+path)
+
+#text processing
+def append_path(path,files):
+    return map(lambda f: path+f,files)
+
+def array_to_txt(array):
+    return reduce(lambda x,y:x+str(y),array,"")
 
 def vector_to_str(vector):
     v=str(vector).replace(" ]","")
@@ -70,11 +58,9 @@ def vector_to_str(vector):
     v=re.sub("[\[\]]","",v)
     return v+"\n"
 
-def read_array(path):
-    arr=read_object(path)
-    arr=map(lambda x:x.flatten(),arr)
-    return np.array(arr)
-
+def get_filename(path):
+    return path.split("/")[-1]
+#images
 def read_images(files):
     return map(lambda f:image.imread(f),files)
 
@@ -85,3 +71,30 @@ def show_images(imgs):
     for img in imgs:
         image.imshow(img)
         time.sleep(1)
+
+def standarize_image(img):
+    return np.reshape(img,(img.size))
+
+#other
+def normalize(data):
+    if(isinstance(data,list)):
+        data=np.array(data)
+    data=data.astype(float)
+    min_max_scaler = preprocessing.MinMaxScaler()
+    data = min_max_scaler.fit_transform(data)
+    return data
+
+def to_csv_file(path,vectors):
+    file_csv = open(path,'w')
+    csv=""
+    for i,instance in enumerate(vectors):
+        print(i)
+        str_v=vector_to_str(instance)
+        csv+=str_v
+    file_csv.write(csv)
+    file_csv.close()
+
+def read_array(path):
+    arr=read_object(path)
+    arr=map(lambda x:x.flatten(),arr)
+    return np.array(arr)
